bea9bc1517daa509c1fd6015117a9562dd44ec5b
Josh-Norton <noshjorton@gmail.com>
2018-10-28 23:26:39 +1300

    Initial commit

---
+++ VueLearning/main.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'These are some nice socks',
+    image: './assets/dog.jpg',
+    link: 'https://www.google.com',
+    inventory: 20,
+    onSale: false,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "Green",
+        variantImage: "./assets/dog.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "Blue",
+        variantImage: "./assets/dog2.jpg"
+      }
+    ],
+    sizes: ["Small", "Medium", "Large"],
+    cart: 0
+  },
+  methods: {
+    addToCart: function () {
+      this.cart++;
+      this.inventory--;
+    },
+    removeFromCart: function () {
+      this.cart--;
+      this.inventory++;
+    },
+    updateProduct: function (variantImage) {
+      this.image = variantImage;
+    }
+  }
+});
+
+// var header = document.querySelector("h1");
+// header.innerHTML = "Does this work?";
